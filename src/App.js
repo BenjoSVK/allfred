0a925c09f7c7f10f532ec388b3fee75865f02327
BenjoSVK <benjami.ptacek@gmail.com>
2024-02-16 20:06:24 +0100

    First attemp using a proxy in package.json file.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function MyComponent() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('/api/people') // Replace with your API endpoint
+    fetch('/api/people')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
